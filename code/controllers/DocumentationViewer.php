27a4f91d70367961234b7331b00be5e5c1df42f4
martimiz <martine@balbus.nl>
2012-11-12 16:19:44 +0100

    Fix: missing 'current' linking mode in top-level menu
    If there are no children, the LinkingMode was set to 'link' and never
    to 'current'

--- code/controllers/DocumentationViewer.php
+++ code/controllers/DocumentationViewer.php
@@ -546,7 +546,10 @@ class DocumentationViewer extends Controller {
 					
 					return $children;
 				}
-			}
+			} else {
+				if ($page->getRelativeLink() == $this->Remaining[$level]) {
+					$page->LinkingMode = 'current';
+				}
 		}
 		
 		return false;
