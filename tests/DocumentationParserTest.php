45be91282c36501aa599759049e50a2f53ddcc6c
Will Rossiter <will@fullscreen.io>
2014-09-28 21:41:10 +1300

    Restore tests

--- tests/DocumentationParserTest.php
+++ tests/DocumentationParserTest.php
@@ -317,11 +317,11 @@ HTML;
 		);
 
 		$this->assertContains(
-			'[link: api](http://api.silverstripe.org/search/lookup/?q=DataObject&version=2.4&module=documentationparsertest)',
+			'[link: api](http://api.silverstripe.org/search/lookup/?q=DataObject&amp;version=2.4&amp;module=documentationparsertest)',
 			$result
 		);
 		$this->assertContains(
-			'[DataObject::$has_one](http://api.silverstripe.org/search/lookup/?q=DataObject%3A%3A%24has_one&version=2.4&module=documentationparsertest)',
+			'[DataObject::$has_one](http://api.silverstripe.org/search/lookup/?q=DataObject::$has_one&amp;version=2.4&amp;module=documentationparsertest)',
 			$result
 		);
 	}
