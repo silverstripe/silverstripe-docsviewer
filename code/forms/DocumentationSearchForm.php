7a7e1bc38a6c29b82b7625ded82ba7e6e36cd696
Cam Findlay <cam@silverstripe.com>
2014-11-28 13:58:12 +1300

    Bug with Search Entities - removing from form. Removed calls to page entity, there is none when in search controller action

--- code/forms/DocumentationSearchForm.php
+++ code/forms/DocumentationSearchForm.php
@@ -3,9 +3,17 @@
 class DocumentationSearchForm extends Form {
 
 	public function __construct($controller) {
+
+		//pass through the current version
+		$page = $controller->getPage();
+		if($page){
+			$versions = HiddenField::create('Versions','Versions',$page->getEntity()->getVersion());
+		}
+		
 		$fields = new FieldList(
 			TextField::create('q', _t('DocumentationViewer.SEARCH', 'Search'), '')
-				->setAttribute('placeholder', _t('DocumentationViewer.SEARCH', 'Search'))
+				->setAttribute('placeholder', _t('DocumentationViewer.SEARCH', 'Search')),
+			$versions
 		);
 		
 		$actions = new FieldList(
